b5f7907f7b99585a399bcbdc6019eb3d3c340226
Sida Liu <learner.sida.liu@gmail.com>
2023-04-30 14:07:02 +0800

    rewrite table people problem

---
+++ khan_table_people_problem_sida.py
@@ -0,0 +1,18 @@
+"""
+    Solve the problem from Khan Academy Math Grade 4
+"""
+
+"""
+    Problem:
+        One table can fit 6 people. but if we put 2 tables together, it can fit 10 people.
+        Let x be the number of tables.
+        Write an equation to represent the number of people that can fit at x tables.
+        For example: How many people can fit at 3 tables?
+        Answer: 18 people
+"""
+
+num_table = int(input("How many tables? "))
+print(f"Number of table: {num_table}")
+assert num_table>0, "Number of table must be greater than 0"
+num_people = 2 + num_table * 4
+print(f"Number of people: {num_people}")
