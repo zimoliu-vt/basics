b5080e58a2a28d858d85b47808dae07467fe9621
Sida Liu <learner.sida.liu@gmail.com>
2023-05-02 13:55:16 +0800

    changing the prompt

--- safe/main.py
+++ safe/main.py
@@ -1,6 +1,6 @@
 import check
 import guessing
-real_pin = input("please give me the password of your safe. thank you!")
+real_pin = input("please give me the password of your safe. thank you!\n(Only four digits):")
 
 check.set_real_pin(real_pin)
 
