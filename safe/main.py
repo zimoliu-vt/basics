1fd887dda88990ea5df94c926d9f33e357824658
zimoliu-vt <131325658+zimoliu-vt@users.noreply.github.com>
2023-05-02 05:42:45 +0000

    ok

--- safe/main.py
+++ safe/main.py
@@ -1,8 +1,9 @@
 import check
 
-num = input("please give me the password of your safe. thank you!")
-num = int(num)
-check.set_real_pin(num)
-answer=check.check(num)
+real_pin = input("please give me the password of your safe. thank you!")
+real_pin = int(real_pin)
+check.set_real_pin(real_pin)
+answer=check.check(real_pin)
 
 assert answer == True, "please check the code"
+
